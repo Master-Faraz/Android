b7845a60deada75a3f64b402bcdb51fba448aa86
Master-Faraz <farazali9028@gmail.com>
2022-05-22 21:07:13 +0530

    Birthday Greeting app

--- Second/app/src/main/java/com/example/second/MainActivity.kt
+++ Second/app/src/main/java/com/example/second/MainActivity.kt
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button2.setOnClickListener()
     }
 
     fun showToast(view: View) {
         Toast.makeText(this , "This is button click " , Toast.LENGTH_SHORT).show()
     }
+
+
 }
--- app/src/main/java/com/example/birthdaygreetingapp/BirthdayGreetingActivity.kt
+++ app/src/main/java/com/example/birthdaygreetingapp/BirthdayGreetingActivity.kt
@@ -2,10 +2,23 @@ package com.example.birthdaygreetingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_birthday_greeting.*
 
 class BirthdayGreetingActivity : AppCompatActivity() {
+
+    companion object{
+        const val  intent_Key = "int_key"
+    }
+    //          For creating constant static key so that it canbe accessed from main function
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_birthday_greeting)
+
+
+        val name = intent.getStringExtra(intent_Key)
+        //     Recieving intent from main file
+
+      textView_id_xml.text = "Happy Birthday \n$name"
     }
 }
